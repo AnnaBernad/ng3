cdc15be487e5b33f24ce2031dbfbbb3310002b19
AnnaBernad <73069123+AnnaBernad@users.noreply.github.com>
2021-01-30 23:20:51 +0200

    .

---
+++ src/data/users.ts
@@ -0,0 +1,13 @@
+export let users = [
+  {id: 1, name: 'vasya', age: 31, status: false, city: 'Lviv'},
+  {id: 2, name: 'petya', age: 30, status: true, city: 'Kyiv'},
+  {id: 3, name: 'kolya', age: 29, status: true, city: 'Lviv'},
+  {id: 4, name: 'olya', age: 28, status: false, city: 'Ternopil'},
+  {id: 5, name: 'max', age: 30, status: true, city: 'Lviv'},
+  {id: 6, name: 'anya', age: 31, status: false, city: 'Kyiv'},
+  {id: 7, name: 'oleg', age: 28, status: false, city: 'Ternopil'},
+  {id: 8, name: 'andrey', age: 29, status: true, city: 'Lviv'},
+  {id: 9, name: 'masha', age: 30, status: true, city: 'Kyiv'},
+  {id: 10, name: 'olya', age: 31, status: false, city: 'Lviv'},
+  {id: 11, name: 'max', age: 31, status: true, city: 'Ternopil'}
+];
